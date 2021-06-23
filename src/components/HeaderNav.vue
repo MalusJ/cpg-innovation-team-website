3921ba874269a6971f218fc4ea7857353364e33c
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-06-23 16:31:06 -0700

    basic header

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,36 +1,43 @@
 <template>
-  <v-app-bar app>
+  <v-app-bar app color="blue lighten-2">
     <template v-slot:img="{ props }">
       <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
     </template>
+
     <v-spacer></v-spacer>
-    <v-app-bar-title>Logo</v-app-bar-title>
+
+    <v-app-bar-title style="align-self: center">Logo</v-app-bar-title>
     <v-spacer></v-spacer>
+
     <v-toolbar-items>
       <v-btn text>Home</v-btn>
       <v-btn text>About</v-btn>
       <v-btn text>Projects</v-btn>
       <v-btn text>Recuitment</v-btn>
       <v-btn text>Team</v-btn>
+      <v-btn text>Contact</v-btn>
     </v-toolbar-items>
 
     <v-spacer></v-spacer>
-    <input type="text" style />
+
+    <input type="text" placeholder="search" />
     <v-btn icon>
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
 
     <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" light v-bind="attrs" v-on="on">中文</v-btn>
+        <v-btn color="primary" light v-bind="attrs" v-on="on">中文<v-icon>mdi-chevron-down</v-icon></v-btn>
       </template>
       <v-list>
-        <v-list-item>
-          <v-list-item-title>中文</v-list-item-title>
-        </v-list-item>
-        <v-list-item>
-          <v-list-item-title>English</v-list-item-title>
-        </v-list-item>
+        <v-list-item-group color="primary">
+          <v-list-item>
+            <v-list-item-content>中文</v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>English</v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-menu>
   </v-app-bar>
@@ -44,6 +51,7 @@ export default {
 
 <style lang="scss">
 input {
-  background-color: white;
+  border-radius: 4px !important;
+  background-color: rgb(232, 237, 255) !important;
 }
 </style>

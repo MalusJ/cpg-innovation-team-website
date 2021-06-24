3451603690d20e7398e315adeb1b2bdc4fbfd209
fork1028 <xinyue_chen@icloud.com>
2021-06-24 11:31:22 +0800

    install vue-baidu-map package

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+import BaiduMap from 'vue-baidu-map';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(BaiduMap, {
+  ak: 'OLlbwIlG5ovWtUBiIMCQXH9jsAWGh54r',
+});
 
 new Vue({
   router,

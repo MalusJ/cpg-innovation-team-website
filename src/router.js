2c917c2485edeff8e4933fb40f9c1fbf03595eb4
fork1028 <xinyue_chen@icloud.com>
2021-06-24 18:02:24 +0800

    add router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './views/Homepage.vue';
+import TeamInfo from './views/TeamInfo.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +12,9 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ './views/AboutUs.vue'),
+    path: '/teamInfo',
+    name: 'TeamInfo',
+    component: TeamInfo,
   },
 ];
 
@@ -24,6 +22,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    return {
+      x: 0,
+      y: 0,
+      behavior: 'smooth',
+    };
+  },
 });
 
 export default router;

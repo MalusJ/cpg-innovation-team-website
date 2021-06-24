5c2ea1cc1be228544945dffc78da362904558e89
fork1028 <xinyue_chen@icloud.com>
2021-06-24 11:32:33 +0800

    clean app.vue and add routers

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './views/Homepage.vue';
+import AboutUs from './views/AboutUs.vue';
+import TeamInfo from './views/TeamInfo.vue';
+import RecruitmentInfo from './views/RecruitmentInfo.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +14,19 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ './views/AboutUs.vue'),
+    path: '/aboutUs',
+    name: 'AboutUs',
+    component: AboutUs,
+  },
+  {
+    path: '/teamInfo',
+    name: 'TeamInfo',
+    component: TeamInfo,
+  },
+  {
+    path: '/recruitmentInfo',
+    name: 'RecruitmentInfo',
+    component: RecruitmentInfo,
   },
 ];
 
